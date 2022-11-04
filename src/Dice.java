f4935db43988b075c103df3a9015728967160321
MatthewDurocher <mattkk177@gmail.com>
2022-11-03 21:35:07 -0400

    Implemented Dice class

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,37 @@
+/*
+@author: Matthew Durocher
+ */
+
+import java.util.Random;
+
 public class Dice {
+    int value;
+    boolean kept;
+    Random number = new Random();
+    public Dice() {
+        this.value = 1;
+        this.kept = false;
+    }
+
+    private int getValue() {
+        return this.value;
+    }
+
+    private boolean isKept(){
+        return this.kept;
+    }
+
+    public void roll() {
+        this.value = number.nextInt(1,6);
+        System.out.println("Die rolled:" + value + "\n");
+    }
+
+    public static void main(String[] args) {
+        Dice myDice = new Dice();
+        myDice.roll();
+
+        System.out.println("Die value is: " + myDice.getValue());
+        System.out.println("Die is kept: " + myDice.isKept());
+    }
+
 }
