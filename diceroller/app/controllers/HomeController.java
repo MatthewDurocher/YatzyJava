bfbeb91c1ed9259bc6d3f732385d8f503e4701e7
MatthewDurocher <mattkk177@gmail.com>
2022-11-06 20:23:12 -0500

    version endpoint implemented

---
+++ diceroller/app/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import play.mvc.*;
+import play.twirl.api.Html;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        return ok(views.html.index.render());
+    }
+
+    public Result applyHtml() {
+        return ok(Html.apply("<h1>This text will appear as a heading 1</h1>"));
+    }
+
+    public Result version() {
+        return ok("{\"appname\":\"diceroller\",\"version\":\"v0.1.0\"}");
+    }
+
+}
---
+++ diceroller/build.sbt
@@ -0,0 +1,15 @@
+name := """diceroller"""
+organization := "com.example"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.13.10"
+
+libraryDependencies += guice
+
+libraryDependencies ++= Seq(
+  "com.google.inject" % "guice" % "5.1.0",
+  "com.google.inject.extensions" % "guice-assistedinject" % "5.1.0"
+)
